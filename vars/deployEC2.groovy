c6adec74c66516ceecb6d9e29195c075995e950e
Darrs08 <87467049+Darrs08@users.noreply.github.com>
2021-10-12 02:26:06 +0800

    Update deployEC2.groovy

--- vars/deployEC2.groovy
+++ vars/deployEC2.groovy
@@ -7,12 +7,12 @@ def call() {
         stages {
             stage('Upload template to S3') {                  
                 steps {
-                    uploadTemplateToS3()
+                    uploadTemplateS3()
                 }
             }
             stage('Deploy EC2') {                  
                 steps {
-                    deployEC2()
+                    deployToEC2()
                 }
             }
         }
