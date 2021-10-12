4fc4749045806e071c8711296b29d5f6128180e2
Darrs08 <87467049+Darrs08@users.noreply.github.com>
2021-10-12 16:41:51 +0800

    Create uploadCertainFile.groovy

---
+++ vars/uploadCertainFile.groovy
@@ -0,0 +1,8 @@
+def call(Map stageParams = [awsRegion: "us-east-1", s3Bucket: "testbucket-darren", certainFile: ""]) {
+    withAWS(region: stageParams.awsRegion, credentials:"${AWS_CRED}") {       
+        s3Upload( 
+            bucket: stageParams.s3Bucket,
+            path: ".txt"                       
+        )
+    }
+}
