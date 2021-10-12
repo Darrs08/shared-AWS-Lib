dca5e1faf16135e7242b672b49c6317b3b5cf9c5
Darrs08 <87467049+Darrs08@users.noreply.github.com>
2021-10-12 17:03:11 +0800

    Update deleteFileS3.groovy

--- vars/deleteFileS3.groovy
+++ vars/deleteFileS3.groovy
@@ -1,5 +1,5 @@
 def call(Map stageParams = [:]) {
-    withAWS(region: "${stageParams.awsRegion}", credentials:"${AWS_CRED}") {       
+    withAWS(region: "${AWS_REGION}", credentials:"${AWS_CRED}") {       
         s3Delete( 
             bucket: "${stageParams.s3Bucket}",
             path: "${stageParams.pathName}"                       
