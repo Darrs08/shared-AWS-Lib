81adf3eea8492f087f099361f57c03ce923065eb
Darrs08 <87467049+Darrs08@users.noreply.github.com>
2021-10-12 16:46:56 +0800

    Update uploadFileS3.groovy

--- vars/uploadFileS3.groovy
+++ vars/uploadFileS3.groovy
@@ -2,7 +2,7 @@ def call(Map stageParams = [:]) {
     withAWS(region: ${stageParams.awsRegion}, credentials:"${AWS_CRED}") {       
         s3Upload( 
             includePathPattern: "**/*",
-            bucket: ${stageParams.bucket}            
+            bucket: ${stageParams.s3bucket}            
         )
     }
 }
