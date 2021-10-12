7a23389b3e070cd0aee5cdb308d3915c6844c569
Darrs08 <87467049+Darrs08@users.noreply.github.com>
2021-10-12 17:03:45 +0800

    Update deployToEC2.groovy

--- vars/deployToEC2.groovy
+++ vars/deployToEC2.groovy
@@ -1,5 +1,5 @@
-def call(Map stageParams = [awsRegion: "us-east-1"]){
-  withAWS(region: stageParams.awsRegion, credentials:"${AWS_CRED}") {
+def call(Map stageParams = [:]){
+  withAWS(region:"${AWS_REGION}", credentials:"${AWS_CRED}") {
         awsIdentity()
         cfnCreateChangeSet(
           stack:'EC2Jenkins-Darren', 
