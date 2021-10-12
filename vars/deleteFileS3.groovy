fe2b68fade21f2bcf0992e51c39038ddbab7b70f
Darrs08 <87467049+Darrs08@users.noreply.github.com>
2021-10-12 16:19:15 +0800

    Update deleteFileS3.groovy

--- vars/deleteFileS3.groovy
+++ vars/deleteFileS3.groovy
@@ -2,7 +2,7 @@ def call(Map stageParams = [awsRegion: "us-east-1", s3Bucket: "testbucket-darren
     withAWS(region: stageParams.awsRegion, credentials:"${AWS_CRED}") {       
         s3Delete( 
             bucket: stageParams.s3Bucket 
-            path: "/HelloWorld.txt",                       
+            path: "s3://stageParams.s3Bucket/HelloWorld.txt",                       
         )
     }
 }
