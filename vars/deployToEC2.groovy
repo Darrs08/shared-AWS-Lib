c33ae16612fe0fa1bac2d853679db66e30391291
Darrs08 <87467049+Darrs08@users.noreply.github.com>
2021-10-13 15:01:12 +0800

    Update deployToEC2.groovy

--- vars/deployToEC2.groovy
+++ vars/deployToEC2.groovy
@@ -2,12 +2,12 @@ def call(Map stageParams = [:]){
   withAWS(region:"${AWS_REGION}", credentials:"${AWS_CRED}") {
         awsIdentity()
         cfnCreateChangeSet(
-          stack:"${stageParams.stackName}", 
+          stack: stageParams.stackName, 
           changeSet:'my-change-set', 
           url:"https://${stageParams.s3Bucket}.s3.amazonaws.com/deployEC2.yml"
         )
         cfnExecuteChangeSet(
-          stack:"${stageParams.stackName}", 
+          stack: stageParams.stackName, 
           changeSet:'my-change-set'
         )
   }
