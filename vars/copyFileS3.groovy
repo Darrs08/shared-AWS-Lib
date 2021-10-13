14a892ae26419d715007995b664276b6be584e77
Darrs08 <87467049+Darrs08@users.noreply.github.com>
2021-10-13 16:54:02 +0800

    Update copyFileS3.groovy

--- vars/copyFileS3.groovy
+++ vars/copyFileS3.groovy
@@ -1,6 +1,6 @@
 def call(Map stageParams = [:]) {
     withAWS(region: "${AWS_REGION}", credentials:"${AWS_CRED}") {       
-        s3Upload( 
+        s3Copy( 
             fromBucket: stageParams.s3fromBucket,
             toBucket: stageParams.s3Bucket,
             fromPath: "src/HelloAbby.txt",                     
