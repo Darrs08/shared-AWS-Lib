bf1619e6e2f758661dbf6613a8b065a135fbbab7
Darrs08 <87467049+Darrs08@users.noreply.github.com>
2021-10-13 14:46:18 +0800

    Update scriptStages.groovy

--- vars/scriptStages.groovy
+++ vars/scriptStages.groovy
@@ -6,19 +6,24 @@ def call() {
             AWS_REGION = 'us-east-1'
         }
         stages {
-            stage('Upload template to S3') {                  
+            stage('Create Bucket') {                  
                 steps {
-                    uploadTemplateS3(s3Bucket:"testbucket-darren")
+                    createBucket()
                 }
             }
-            stage('Upload Certain File to S3') {                  
+            stage('Upload Files to S3') {                  
                 steps {
-                    uploadCertainFileS3(s3Bucket:"testbucket-darren", certainFile: "CertainFileSample.txt")
+                    uploadAllFileS3(s3Bucket:"sample-bucket-darren")
                 }
             }
+            /*stage('Upload Certain File to S3') {                  
+                steps {
+                    uploadCertainFileS3(s3Bucket:"sample-bucket-darren", certainFile: "CertainFileSample.txt")
+                }
+            }*/
             stage('Delete a file from S3 bucket') {                  
                 steps {
-                    deleteFileS3(s3Bucket: "testbucket-darren", pathName: "HelloWorld.txt")
+                    deleteFileS3(s3Bucket: "sample-bucket-darren", pathName: "HelloWorld.txt")
                 }
             }
             stage('Deploy EC2') {                  
